86b2aa768ecfa1330a6de6ba5f0b21dbc7a164c8
Sasha <Oleksbielianinov@gmail.com>
2016-07-03 23:32:42 +0300

    Add google map

--- js/_assets-sprockets.js
+++ js/_assets-sprockets.js
@@ -1,2 +1,18 @@
 //@prepros-append ../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
 
+function initialize() {
+        var mapProp = {
+            center: new google.maps.LatLng(49.812013, -97.232792),
+            zoom: 10,
+            disableDefaultUI:true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        var marker = new google.maps.Marker({
+            position: myCenter,
+            icon: 'images/map-marker.png'
+        });
+
+        marker.setMap(map);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
